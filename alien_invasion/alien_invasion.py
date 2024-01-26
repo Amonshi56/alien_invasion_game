c02339c01bdb1d899b3ca71c415bb02b6e988441
Stanislav Krikunov <krikunov.stas@yandex.ru>
2024-01-26 13:56:37 +0900

    Перемещение и рефакторинг
    Добавлена функциональность перемещения корабля, настройки скорости, ограничение передвижения рамками. Рефакторинг метода _check_events с вынесением в отдельные методы  функционала keyup и keydown. Удалены лишние файлы из каталога с проектами

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     '''Класс для управления ресурсами и поведением игры.'''
@@ -15,19 +16,48 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
+        self.ship = Ship(self)
+
     def run_game(self):
         '''Запуск основного цикла игры.'''
         while True:
-            # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.settings.bg_color)
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-            # Отоброжение последнего прорисованного экрана.
-            pygame.display.flip()
+    def _check_events(self):
+        # Отслеживание событий клавиатуры и мыши.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)   
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        '''Реакция на нажатие клавиш.'''
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()    
+
+    def _check_keyup_events(self, event):
+        '''Реакция на отпускание клавиш.''' 
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False                               
+
+    def _update_screen(self):
+        # При каждом проходе цикла перерисовывается экран.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Отоброжение последнего прорисованного экрана.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
