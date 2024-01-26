0c238668a61c9c785a8269d2fe7998901b820cf5
Stanislav Krikunov <krikunov.stas@yandex.ru>
2024-01-26 17:22:06 +0900

    Стрельба и пули
    Реализована стрельба, исчезновения пуль, Отдельный модуль с настройками стрельбы bullet.py подключаемое ограничение пуль на экране.

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -5,9 +5,18 @@ class Settings():
     def __init__(self):
         '''Инициализирует настройки игры.'''
         # Параметры экрана
-        self.screen_width = 1200
+        self.screen_width = 800
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        self.fullscreen = True
+        self.bullet_limit = False
+        self.bullets_allowed = 3
 
         # Настройки корабля
         self.ship_speed = 1.5
+
+        # Параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (255, 0, 0)
